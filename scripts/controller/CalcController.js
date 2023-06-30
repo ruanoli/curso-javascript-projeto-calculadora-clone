73762e3037462f9a01d3a60ceff858e36c4e91cb
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2023-06-29 21:32:59 -0300

    DOM

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,7 +3,18 @@ class CalcController{
     {
         //atributos com underline são considerados privados - é uma convensão.
         this._displayCalc = "0";
-        this._dataAtual;
+        this.currentDate;
+        this.initializer();
+    }
+
+    initializer(){
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "256";
+        dateEl.innerHTML = "25/01/18";
+        timeEl.innerHTML = "00:02";
     }
 
     get displayCalc(){
@@ -15,10 +26,10 @@ class CalcController{
     }
 
     get dataAtual(){
-        return this._dataAtual;
+        return this.currentDate;
     }
 
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set dataAtual(value){
+        this.currentDate = value;
     }
 }
