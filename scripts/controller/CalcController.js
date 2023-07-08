8a361a69f3d00e0231a5a8c7df52f6ecc371a52f
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2023-07-08 20:11:14 -0300

    metodos de acao

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,6 +3,7 @@ class CalcController{
     {
         //atributos com underline são considerados privados - é uma convensão.
         this._locale = 'pt-BR';
+        this._operation = [];
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
@@ -22,14 +23,84 @@ class CalcController{
         }, 1000);
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+
+        console.log(this._operation);
+    }
+
+    excBtn(value){
+        switch(value){
+            case 'ac':
+                this.clearAll()
+                break;
+            case 'ce':
+                this.clearEntry()
+                break;
+            case 'soma':
+                break;
+            case 'subtracao':
+                break;
+            case 'divisao':
+                break;
+            case 'multiplicacao':
+                break;
+            case 'porcento':
+                break;
+            case 'igual':
+                break;
+                
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
+    }
+
     initButtonsEvents(){
         //pega todos os "g" que estão dentro do id button e do id parts.
         var buttons = document.querySelectorAll("#buttons > g, #parts > g");
-
+        
         buttons.forEach((btn, index) => {
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn,'click drag', e => {
             
                 console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+
+                btn.style.cursor = "pointer";
+
             });
         });
     }
